0db489bc251e5d4c09d78375b201b83395f6107a
tejas-ambalia <124034814+tejas-ambalia@users.noreply.github.com>
2024-04-23 17:36:36 +0530

    automation flow

---
+++ get_changes.sh
@@ -0,0 +1,28 @@
+read -p "Enter branch name to pull from tm-givsum repo:" tm_branchname
+if [ ! -z "$tm_branchname" ]
+then
+	cd /home/tejasambalia/Documents/GitHub/tm-givsum
+	checkout_output=$(git checkout $tm_branchname)
+	if [[ $checkout_output == *"error"* ]]; then
+		echo "$checkout_output"
+	else
+		git pull
+		cd /home/tejasambalia/Documents/GitHub-master/givsum
+		check_live_branch=$(git branch -a | grep -i $tm_branchname)
+		if [ -z "$check_live_branch" ]
+		then
+			echo "IF"
+			git checkout -b $tm_branchname main
+		else
+			echo "ELSE"
+			git checkout $checkout_output
+			git pull
+		fi
+		cp -a /home/tejasambalia/Documents/GitHub/tm-givsum/app/. /home/tejasambalia/Documents/GitHub-master/givsum/app
+		cp -a /home/tejasambalia/Documents/GitHub/tm-givsum/db/. /home/tejasambalia/Documents/GitHub-master/givsum/db
+		cp -a /home/tejasambalia/Documents/GitHub/tm-givsum/config/. /home/tejasambalia/Documents/GitHub-master/givsum/config
+	fi
+	# git branch --all | grep -i $tm_branchname >> /home/tejasambalia/Documents/automation/outpui.txt
+else
+	echo "Please re-run script and enter branch name"
+fi
---
+++ send_changes.sh
@@ -0,0 +1,9 @@
+cd /home/tejasambalia/Documents/GitHub/tm-givsum
+git checkout main
+git pull
+cd /home/tejasambalia/Documents/GitHub-master/givsum
+git checkout main
+git pull
+cp -a /home/tejasambalia/Documents/GitHub-master/givsum/app/. /home/tejasambalia/Documents/GitHub/tm-givsum/app
+cp -a /home/tejasambalia/Documents/GitHub-master/givsum/db/. /home/tejasambalia/Documents/GitHub/tm-givsum/db
+cp -a /home/tejasambalia/Documents/GitHub-master/givsum/config/. /home/tejasambalia/Documents/GitHub/tm-givsum/config
