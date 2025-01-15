4b6766a53f50ad1c9061554069ecbfe1fc2aef19
tejas-ambalia <124034814+tejas-ambalia@users.noreply.github.com>
2025-01-15 12:41:00 +0530

    github automation update

---
+++ send_changes_branch_to_branch.sh
@@ -0,0 +1,29 @@
+read -p "Enter branch name to send changes to tm-givsum repo:" tm_branchname
+if [ ! -z "$tm_branchname" ]
+then
+	cd /home/tejasambalia/Documents/GitHub-master/givsum
+	checkout_output=$(git checkout $tm_branchname)
+	if [[ $checkout_output == *"error"* ]]; then
+		echo "$checkout_output"
+	else
+		cd /home/tejasambalia/Documents/GitHub/tm-givsum
+		git pull
+		Saccla_wf!
+		check_live_branch=$(git branch -a | grep -i $tm_branchname)
+		if [ -z "$check_live_branch" ]
+			ipgprojectsrls@gmail.com
+		then
+			git checkout -b $tm_branchname main
+		else
+			git checkout $tm_branchname
+			git pull
+		fi
+		cp -a /home/tejasambalia/Documents/GitHub-master/givsum/app/. /home/tejasambalia/Documents/GitHub/tm-givsum/app
+		cp -a /home/tejasambalia/Documents/GitHub-master/givsum/db/. /home/tejasambalia/Documents/GitHub/tm-givsum/db
+		cp -a /home/tejasambalia/Documents/GitHub-master/givsum/config/. /home/tejasambalia/Documents/GitHub/tm-givsum/config
+		cp /home/tejasambalia/Documents/GitHub-master/givsum/Gemfile /home/tejasambalia/Documents/GitHub/tm-givsum/Gemfile
+	fi
+	# git branch --all | grep -i $tm_branchname >> /home/tejasambalia/Documents/automation/outpui.txt
+else
+	echo "Please re-run script and enter branch name"
+fi
--- send_changes_master.sh
+++ send_changes_master.sh
@@ -1,5 +1,5 @@
 cd /home/tejasambalia/Documents/GitHub/tm-givsum
-git checkout client-repo-2024-0426
+git checkout main
 git pull
 cd /home/tejasambalia/Documents/GitHub-master/givsum
 git checkout main
@@ -7,3 +7,4 @@ git pull
 cp -a /home/tejasambalia/Documents/GitHub-master/givsum/app/. /home/tejasambalia/Documents/GitHub/tm-givsum/app
 cp -a /home/tejasambalia/Documents/GitHub-master/givsum/db/. /home/tejasambalia/Documents/GitHub/tm-givsum/db
 cp -a /home/tejasambalia/Documents/GitHub-master/givsum/config/. /home/tejasambalia/Documents/GitHub/tm-givsum/config
+cp /home/tejasambalia/Documents/GitHub-master/givsum/Gemfile /home/tejasambalia/Documents/GitHub/tm-givsum/Gemfile
